2d0359825fcff50131c8ceab26388ff50e621f8a
Opilane TTHK <opilane@tthk.ee>
2020-09-15 13:56:39 +0300

    Create Web Page

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WeekDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is it today?");
+            string day = Console.ReadLine().ToLower();
+
+            {
+                if (day == "monday" || day == "tuesday" || day == "wendesday" || day == "thursday" || day == "friday")
+                {
+                    Console.WriteLine("Today is workday");
+                }
+                else if (day == "saturday" || day == "sunday")
+                {
+                    Console.WriteLine("Today is a restday");
+                }
+                else
+                    Console.WriteLine("You didint eneter a day");
+                    
+                    
+            }
+
+               
+            
+            
+
+
+        }
+
+    }
+}
